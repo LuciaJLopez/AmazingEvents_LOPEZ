978db94b9f509d804e7870acbdb09a7ecc911bed
= <=>
2023-03-15 02:33:41 -0300

    se realizaron cambios

--- JS/details.js
+++ JS/details.js
@@ -9,6 +9,21 @@ const event = data.events.find((event) => event._id == cardsID);
 
 console.log(event);
 
+cardDetail.innerHTML = `
+  <div class="d-flex flex-column align-items-center justify-content-center">
+    <img src="${event.image}" class="card-img-top" alt="${event.name}">
+    <div class="card-body d-flex flex-column align-items-center justify-content-center">
+      <h5 class="card-title fw-bold">${event.name}</h5>
+      <p class="card-text">${event.description}</p>
+      <p class="card-text">Date: ${event.date}</p>
+      <p class="card-text">Place: ${event.place}</p>
+      <h8>Price: ${event.price}</h8>
+    </div>
+  </div>
+`;
+
+
+/*
 function createDetails(event, container){
     let div = document.createElement('div')
     div.classList = 'col-md-8 '
@@ -23,4 +38,5 @@ function createDetails(event, container){
     container.appendChild(div);
 }
 
-createDetails(evento, detailContainer);
+createDetails(event, detailContainer);
+*/
